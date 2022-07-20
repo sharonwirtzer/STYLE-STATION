5d3e057dcfa85765266c988a9e9848b95e683810
sharonwirtzer <sharonwirtzer@gmail.com>
2022-07-21 01:31:28 +0300

    sign-up

--- error.php
+++ error.php
@@ -1,39 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="icon" type="image/png" href="img/pole.png" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/error.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="error.css">
+    <title>error page</title>
 </head>
 <body>
-<!--
-VIEW IN FULL SCREEN MODE
-FULL SCREEN MODE: http://salehriaz.com/404Page/404.html
-
-DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
--->
 
 <body class="bg-purple">
         
         <div class="stars">
             <div class="custom-navbar">
-                <div class="brand-logo">
+<!--                 <div class="brand-logo">
                     <img src="http://salehriaz.com/404Page/img/logo.svg" width="80px">
-                </div>
-                <div class="navbar-links">
+                </div> -->
+<!--                 <div class="navbar-links">
                     <ul>
                       <li><a href="http://salehriaz.com/404Page/404.html" target="_blank">Home</a></li>
                       <li><a href="http://salehriaz.com/404Page/404.html" target="_blank">About</a></li>
                       <li><a href="http://salehriaz.com/404Page/404.html" target="_blank">Features</a></li>
                       <li><a href="http://salehriaz.com/404Page/404.html" class="btn-request" target="_blank">Request A Demo</a></li>
                     </ul>
-                </div>
+                </div> -->
             </div>
             <div class="central-body">
                 <img class="image-404" src="http://salehriaz.com/404Page/img/404.svg" width="300px">
-                <a href="http://salehriaz.com/404Page/404.html" class="btn-go-home" target="_blank">GO BACK HOME</a>
+                <a href="index.php" class="btn-go-home" >GO BACK HOME</a>
             </div>
             <div class="objects">
                 <img class="object_rocket" src="http://salehriaz.com/404Page/img/rocket.svg" width="40px">
@@ -57,6 +52,5 @@ DRIBBBLE: https://dribbble.com/shots/4330167-404-Page-Lost-In-Space
         </div>
 
     </body>
-<script src="/error.js"></script>
 </body>
 </html>
