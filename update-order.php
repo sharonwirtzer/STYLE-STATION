5ba9dc121b8cde86c0578a3af6d8b94bfa2cef57
sharonwirtzer <sharonwirtzer@gmail.com>
2022-07-23 16:31:25 +0300

    update

--- update-order.php
+++ update-order.php
@@ -40,8 +40,7 @@ if (isset($_SESSION['user']))
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/main.css">
-  <link rel="stylesheet" href="css/details.css">
+  <link rel="stylesheet" href="css/update.css">
   <title>client</title>
 
 </head>
