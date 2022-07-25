3eab343699e7eddfb911b06712bd2fc533bb2998
sharonwirtzer <sharonwirtzer@gmail.com>
2022-07-25 21:20:52 +0300

    about

--- add-order.php
+++ add-order.php
@@ -66,7 +66,7 @@ if (isset($_SESSION['user']))
       </div>
       <div>
         <input type="date" name="date" id="date" required>
-        <input id="date_picker" type="date">
+        <input id="datefield" type='date' min='1899-01-01' max='2000-13-13'></input>
         <input type="time" name="time" id="time" required>
       </div>
       <input type="text" placeholder="הכנס תעודת זהות" name="idcard" id="idcard" required>
@@ -120,13 +120,23 @@ if (isset($_SESSION['user']))
   </div>
 
   <script>
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0');
-  var yyyy = today.getFullYear();
 
-  today = yyyy + '-' + mm + '-' + dd;
-  $('#date_picker').attr('min', today);
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+   dd = '0' + dd;
+}
+
+if (mm < 10) {
+   mm = '0' + mm;
+} 
+    
+today = yyyy + '-' + mm + '-' + dd;
+document.getElementById("datefield").setAttribute("max", today);
+
 </script>
 
 
