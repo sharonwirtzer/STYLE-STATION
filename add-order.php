a2a91db6332329db3991b1a79344bcde168e46c1
sharonwirtzer <sharonwirtzer@gmail.com>
2022-07-25 21:47:23 +0300

    about

--- add-order.php
+++ add-order.php
@@ -66,7 +66,7 @@ if (isset($_SESSION['user']))
       </div>
       <div>
         <input id="datefield" type='date' min='1899-01-01' max='2000-13-13' required>
-        <input type="time" name="time" id="time"   step="300"
+        <input type="time" name="time" id="time"   
             min="09:00"
             max="22:00" required>
         
