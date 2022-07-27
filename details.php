7a4a8e1473c27590de72694c1488aba9090e6eb1
sharonwirtzer <sharonwirtzer@gmail.com>
2022-07-27 22:30:25 +0300

    about

--- details.php
+++ details.php
@@ -32,6 +32,7 @@ if (isset($_SESSION['user']))
         <li id="bar"><a href="about.html">About</a></li>
         <li id="bar" style="float: right"><a href="Dashboards.html">Dashboards</a></li>
         <li id="bar" style="float: right; "><a href="add-order.php">My Orders</a></li>
+        <li id="bar" style="float: right; "><a href="Employee.php">Employee</a></li>
         <li id="bar" style="float: left; margin-right: 30px; font-size: 20px; "><a href="details.php"><img src="img/pole.png" width="15px">STYLE STATION</a></li>
         <?php if (isset($_SESSION['user']))
             echo "<p>Hello, $first_name | <a href='log-out.php' class='log-out'>Log Out</a></p>"; ?>
